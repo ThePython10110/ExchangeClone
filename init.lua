6e8f243cef5ea89b7d653b99362001ccd3a46563
Leo Spratt <enchant97@users.noreply.github.com>
2021-08-25 17:01:36 +0100

    add orb & deconstructor

---
+++ init.lua
@@ -0,0 +1,4 @@
+local default_path = minetest.get_modpath("element_exchange")
+
+dofile(default_path.."/deconstructor.lua")
+dofile(default_path.."/orb.lua")
---
+++ orb.lua
@@ -0,0 +1,21 @@
+function read_orb_charge(itemstack, user, pointed_thing)
+    local stored = itemstack:get_meta():get_int("stored_charge") or 0
+    minetest.chat_send_player(user:get_player_name(), "Current Charge: "..stored)
+    return itemstack
+end
+
+minetest.register_tool("element_exchange:exchange_orb", {
+    description = "Exchange Orb",
+    inventory_image = "ee_exchange_orb.png",
+    on_use = read_orb_charge,
+})
+
+minetest.register_craft({
+    type = "shaped",
+    output = "element_exchange:exchange_orb",
+    recipe = {
+        {"", "default:diamond",""},
+        {"default:diamond", "default:gold", "default:diamond"},
+        {"", "default:diamond",  ""}
+    }
+})
