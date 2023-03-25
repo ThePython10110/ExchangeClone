2badbed42402f054313781a1aa0ba8dbdf2657d2
ThePython <nolanjbowers@gmail.com>
2023-03-25 10:58:08 -0700

    Formspec update that will probably fail

--- deconstructor.lua
+++ deconstructor.lua
@@ -1,12 +1,25 @@
 function get_element_deconstructor_formspec()
-    local formspec = {
-        "size[8,9]",
-        "label[2,1;Fuel]",
-        "list[context;fuel;2,2;1,1;]",
-        "label[5,1;Orb]",
-        "list[context;dst;5,2;1,1;]",
-        "list[current_player;main;0,5;8,4;]",
-    }
+    if not exchangeclone.mineclone then
+        local formspec = {
+            
+            "size[8,9]",
+            "label[2,1;Fuel]",
+            "list[context;fuel;2,2;1,1;]",
+            "label[5,1;Orb]",
+            "list[context;dst;5,2;1,1;]",
+            "list[current_player;main;0,5;8,4;]",
+        }
+    else
+        local formspec = "size[8,9]"..
+            "no_prepend[]"..
+            mcl_vars.gui_nonbg..mcl_vars.gui_bg_color..
+            "background[-0.19,-0.25;10.5,9.87]"..
+            "label[2,1;"..minetest.formspec_escape(minetest.colorize("#313131", S("Fuel"))).."]"..
+            "listring[context;fuel;2,2;1,1;]"..
+            "label[5,1;"..minetest.formspec_escape(minetest.colorize("#313131", S("Orb"))).."]"..
+            "listring[context;dst;5,2;1,1;]"..
+            "listring[current_player;main;0,5;8,4;]"
+    end
     return table.concat(formspec, "")
 end
 
